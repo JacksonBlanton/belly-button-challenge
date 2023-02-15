4aea8a634614ae6a80e77d106a891fe455e04600
Jackson Blanton <Jackson@Jacksons-Air.lan>
2023-02-15 16:48:33 -0600

    updated app.js

--- Starter_Code 4/StarterCode/static/js/app.js
+++ Starter_Code 4/StarterCode/static/js/app.js
@@ -1,48 +1,46 @@
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-
-let sHovertext = [];
-let sValues = [];
-let sLabels = [];
-let samples = [];
-
-// Fetch  JSON data
-let data = d3.json(url);
-
-data.then(function(data_filters) {
-
-    // Pull first subject's data
-    sValues = data_filters.samples[0].sample_values;
-    sLabels = data_filters.samples[0].otu_labels;
-    sHovertext = data_filters.samples[0].otu_ids;
+// Use D3 library to read in samples.json
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(data => {
+  
+  // Create a function to generate the horizontal bar chart
+  function generateBarChart(sampleId) {
+    // Filter the data to get the selected sample
+    var sampleData = data.samples.filter(sample => sample.id == sampleId)[0];
     
-    // Make list of dictionaries for samples 
-    samples = data_filters.samples;
-
-
-    // Build initial plot using a function
-    function init() {
-        let data = [{
-            y: sValues,
-            x: sHovertext,
-            type: "bar",
-            orientation: "h",
-            hovertext: sLabels
-      
-        }]
-
-    // make initial plot
-        Plotly.newPlot("BarChart", data)}
-    });
-
-    // create drop down m enu
-    let dropDownMenu = data_filters["samples"].map(item => item.id);
-
-    // Add the options 
-    for (let i =0; i< dropDownMenu.length; i++) {
-        var x = document.getElementById("selDataset");
-        var option = document.createElement("option");
-        option.text = dropDownMenu[i];
-        option.value = dropDownMenu[i];
-        x.add(option);
-      
+    // Get the top 10 OTUs and reverse the order for the bar chart
+    var top10Values = sampleData.sample_values.slice(0, 10).reverse();
+    var top10Ids = sampleData.otu_ids.slice(0, 10).reverse().map(id => "OTU " + id);
+    var top10Labels = sampleData.otu_labels.slice(0, 10).reverse();
+    
+    // Create the bar chart
+    var trace1 = {
+      x: top10Values,
+      y: top10Ids,
+      text: top10Labels,
+      type: "bar",
+      orientation: "h"
     };
+    var data1 = [trace1];
+    var layout1 = {
+      title: "Top 10 OTUs",
+      xaxis: { title: "Sample Values" },
+      yaxis: { title: "OTU IDs" }
+    };
+    Plotly.newPlot("bar", data1, layout1);
+  }
+  
+  // Create the dropdown menu
+  var dropdown = d3.select("#selDataset");
+  data.names.forEach(name => {
+    dropdown.append("option").text(name).property("value", name);
+  });
+  
+  // Call the generateBarChart function with the initial sample ID
+  generateBarChart(data.names[0]);
+  
+  // Create an event listener for the dropdown menu
+  dropdown.on("change", function() {
+    var selectedSample = d3.select(this).property("value");
+    generateBarChart(selectedSample);
+  });
+  
+}).catch(error => console.log(error));
